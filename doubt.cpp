0a0b0201a7f6aa201ed79e14ce2e9c4209b7c5ed
ayushatindra <116113390+ayushatindra@users.noreply.github.com>
2024-04-14 00:00:36 +0530

    Add files via upload

---
+++ doubt.cpp
@@ -0,0 +1,123 @@
+#include <iostream>
+using namespace std;
+int main() {
+  int x=10,y=20,z=30;
+  int* arr[]={&x,&y,&z};
+  cout<<*arr[1]<<endl;
+  // *arr[0]=&x;
+  // *arr[1]=&y;
+  // *arr[2]=&z;
+  // int a=10;
+  // int* ptr =&a ;
+  // cout<<*ptr+2**ptr-3**ptr;
+  
+   // char* ptr="ayush";
+  // cout<<*ptr<<endl;
+  // int arr[]={1,2,3,4};
+  // arr=arr+1;
+  // cout<<arr[0]<<endl;
+  
+  
+  
+// int a[]={1,2 ,3,4,5};
+//   int *p=a;
+//   // void* c=(void*) a;
+//   // cout<<*c<<endl;
+//   cout<<*(p+ )<<endl; 
+
+  
+  // int arr[]={1,2,3,4,5};
+  // // int n=sizeof(arr)/sizeof(arr[0]);
+  // int *ptr=arr;
+  // ptr =ptr+6;
+  // cout<<*ptr<<endl;
+  
+  // int b=5;
+  // int* a=&b;
+  // *a=*a+5;
+  // cout<<b<<endl;
+  // cout<<*a<<endl;
+  // a=a+1;
+  // cout<<*a<<endl;
+ // int* ptr =NULL;
+  // cout<<ptr<<endl;
+  // cout<<*ptr<<endl;
+  //    char* ptr="ayush";
+    // pointer 2
+  //    
+  //    int a =5;
+  //    int* p=&a;
+  //    cout<<p<<endl;
+  //    cout<<&p<<endl;
+  //    cout<<*p<<endl;
+  //    solve1(p);
+  //    cout<<p<<endl;
+  //    cout<<&p<<endl;
+  //    cout<<*p<<endl;
+  ////
+      int a =6;
+      cout<<2a;
+  //    int a =10;
+  //    int* p=&a;
+  //    int* q =&a;
+  //    pointer to a pointer
+  //    int a=5;
+  //    int* ptr=&a;
+  //    int** ptr2=&ptr;
+  //    cout<<a<<endl;
+  //    cout<<&a<<endl;
+  //    cout<<ptr<<endl;
+  //    cout<<&ptr<<endl;
+  //    cout<<*ptr<<endl;
+  //    cout<<ptr2<<endl;
+  //    cout<<&ptr2<<endl;
+  //    cout<<*ptr2<<endl;
+  //
+  //
+  //
+  //    int arr[5]={10,20,30};
+  ////    cout<<arr<<endl;
+  ////    cout<<&arr<<endl;
+  ////    int size =3;
+  //    int size =3;
+  //    solve2(arr,size);
+  ////    
+  //    for(int i=0;i<3;i++){
+  //        cout<<arr[i]<<" ";
+  //    }
+  //  int arr[5]={1,2,3,4,5};
+  //    int size=5;
+  ////  cout<<"Size of arr "<<sizeof(arr);
+  //    cout<<endl;
+  //    solve(arr,size);
+    // int arr[]={10,20,30,40};
+    // int size=4;
+    // cout<<sizeof(arr);
+    // solve(arr,4);
+
+  //   int arr[5]={1,2,3,4,5};
+  //   int (*ptr)[5]=&arr;
+  //   // int *ptr1=arr;
+  // for(int i=0;i<5;i++){
+  //   cout<<(*ptr)[i]<<endl;
+  // }
+
+    // int a=5;
+    // int *ptr=&a;
+  //    int arr[5]={1,2,3,4,5};
+  //      cout<<"Size of arr"<<sizeof(arr);
+        // int arr[]={10,20,30,40};
+        // int size=4;
+        // cout<<sizeof(arr);
+        // solve(arr,4);
+
+      //   int arr[5]={1,2,3,4,5};
+      //   int (*ptr)[5]=&arr;
+      //   // int *ptr1=arr;
+      // for(int i=0;i<5;i++){
+      //   cout<<(*ptr)[i]<<endl;
+      // }
+
+        // int a=5;
+        // int *ptr=&a;
+}
